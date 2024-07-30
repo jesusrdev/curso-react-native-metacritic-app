f4336906a41ab538a8a5c9e033e5f483217cc283
Jesus Rojas <rojasalarcone@gmail.com>
2024-07-29 19:14:43 -0500

    using SafeAreaProvider for the paddings

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import Constants from "expo-constants";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Pressable,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
-import { getLatestGames } from "./lib/metacritic";
+import { StyleSheet, View } from "react-native";
+import { Main } from "./components/Main";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    getLatestGames().then((games) => setGames(games));
-  }, []);
-
   return (
-    // View lleva display flex por defecto
-    <View style={styles.container}>
-      {/* Si yo coloco el fondo dark, entonces tengo que cambiar el style del StatusBar. Es un componente de Expo */}
-      <StatusBar style="light" />
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
 
-      <ScrollView>
-        {games.map((game) => (
-          <View key={game.slug} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
-            <Text style={styles.title}>{game.title}</Text>
-            <Text style={styles.score}>{game.score}</Text>
-            <Text style={styles.description}>{game.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -45,34 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: Constants.statusBarHeight,
     padding: 12,
-  },
-
-  card: {
-    marginBottom: 48,
-  },
-  image: {
-    widht: 107,
-    height: 147,
-    borderRadius: 10,
-    resizeMode: "contain",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 10,
-    color: "#fff",
-    marginTop: 10,
-  },
-  description: {
-    fontSize: 16,
-    color: "#eee",
-  },
-  score: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginTop: 10,
   },
 });
