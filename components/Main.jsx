15e7e54b01c7fb93e45751d3a8dc1df382332c9f
Jesus Rojas <rojasalarcone@gmail.com>
2024-07-29 19:42:30 -0500

    using svgs in react native with a dependency

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,6 +3,7 @@ import { View, FlatList, ScrollView, ActivityIndicator } from "react-native";
 import { getLatestGames } from "../lib/metacritic";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import GameCard from "./GameCard";
+import { Logo } from "./Logo";
 
 export function Main() {
   const [games, setGames] = useState([]);
@@ -14,6 +15,9 @@ export function Main() {
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
       {games.length === 0 ? (
         <ActivityIndicator color={"#fff"} size={"large"} />
       ) : (
