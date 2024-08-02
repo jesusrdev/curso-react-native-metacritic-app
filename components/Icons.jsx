b71919461e564b95bae50a39ee2d7e8b47ca880a
Jesus Rojas <rojasalarcone@gmail.com>
2024-08-01 22:09:43 -0500

    adding tabs to the navigations

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -6,5 +6,9 @@ export const CircleInfoIcon = (props) => (
 );
 
 export const HomeIcon = (props) => (
-  <FontAwesome name="home" size={24} color="white" {...props} />
+  <FontAwesome name="home" size={32} color="white" {...props} />
+);
+
+export const InfoIcon = (props) => (
+  <FontAwesome name="info" size={32} color="white" {...props} />
 );
